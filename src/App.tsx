bde53292404bbafffb6108a5ec8910ae5fbb3d19
Christian <christianlindeneg@gmail.com>
2021-03-18 19:29:06 +0100

    routes and footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Auth from './user/pages/Auth';
 import { AuthContext } from './common/context';
 import { useAuth, IAuthHook } from './common/hooks';
 import { Functional } from './common/util';
+import classes from './App.module.css';
+import { Fragment } from 'react';
 
 const App: Functional = () => {
     const { token, login, logout, userId }: IAuthHook = useAuth();
@@ -16,26 +18,45 @@ const App: Functional = () => {
                 <main style={{ marginTop: '5rem' }}>
                     {token ? (
                         <Switch>
+                            <Route path="/" exact>
+                                <Auth />
+                            </Route>
                             <Route path="/boards" exact>
                                 <UserBoards />
                             </Route>
-                            <Route path="/board/:boardId" exact>
-                                {/* Board */}
-                            </Route>
                             <Route path="/profile" exact>
-                                {/* User Profile */}
+                                <div>PROFILE</div>
+                            </Route>
+                            <Route path="/board/:boardId" exact>
+                                <div>BOARD</div>
                             </Route>
                             <Redirect to="/boards" />
                         </Switch>
                     ) : (
-                        <Switch>
+                        <Fragment>
                             <Route path="/" exact>
                                 <Auth />
                             </Route>
                             <Redirect to="/" />
-                        </Switch>
+                        </Fragment>
                     )}
                 </main>
+                <footer className={classes.Footer}>
+                    <span>
+                        <span className={classes.Clickable}>
+                            <a href="https://www.lindeneg.org" target="_blank" rel="noreferrer">
+                                christian lindeneg
+                            </a>
+                        </span>
+                        @ 2021
+                    </span>
+                    -
+                    <span className={classes.Clickable}>
+                        <a href="https://github.com/lindeneg/arranger-frontend" target="_blank" rel="noreferrer">
+                            source-code
+                        </a>
+                    </span>{' '}
+                </footer>
             </BrowserRouter>
         </AuthContext.Provider>
     );
