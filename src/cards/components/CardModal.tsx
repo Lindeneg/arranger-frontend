44c1906fdb697f65aa81c45bad6e1738350166aa
Christian <christianlindeneg@gmail.com>
2021-03-21 23:22:00 +0100

    begin card implementation

--- src/cards/components/CardModal.tsx
+++ src/cards/components/CardModal.tsx
@@ -1,6 +1,99 @@
-// TODO
+import { Fragment, useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const CardModal = () => <div></div>
+import Modal from '../../common/components/Interface/Modal';
+import Button from '../../common/components/Interactable/Button';
+import Card from '../../common/components/Interface/Card';
+import ErrorModal from '../../common/components/Interface/Modal/ErrorModal';
+import Spinner from '../../common/components/Interface/Spinner';
+import { useForm, useHttp, Entry } from '../../common/hooks';
+import { AuthContext, ThemeContext, IAuthContext, IThemeContext } from '../../common/context';
+import {
+    BaseProps,
+    CardResponse,
+    ChecklistResponse,
+    Clickable,
+    Functional,
+    OnSubmitFunc,
+    Visibility
+} from '../../common/util';
+import classes from './CardModal.module.css';
 
+interface CardModalProps extends BaseProps, Visibility, Clickable {
+    cardId: string | null;
+}
+
+type FetchedCard = CardResponse<ChecklistResponse[]>;
+
+const CardModal: Functional<CardModalProps> = (props) => {
+    const history = useHistory();
+    const [currentCard, setCurrentCard] = useState<FetchedCard | null>(null);
+    const authContext = useContext<IAuthContext>(AuthContext);
+    const themeContext = useContext<IThemeContext>(ThemeContext);
+    const { isLoading, error, clearError, sendRequest } = useHttp<FetchedCard>();
+    const [inputState, inputHandler, setInputState] = useForm({
+        inputs: {
+            name: { value: '', isValid: false },
+            color: { value: themeContext.color, isValid: true }
+        },
+        isValid: false
+    });
+
+    useEffect(() => {
+        (async () => {
+            if (!!props.cardId) {
+                try {
+                    // fetch card
+                } catch (err) {}
+            }
+        })();
+    }, [props.cardId]);
+
+    const onDeleteHandler = () => {
+        console.log('DELETE');
+    };
+
+    const onSubmitHandler: OnSubmitFunc = (e) => {
+        e.preventDefault();
+        console.log('SUBMIT');
+    };
+
+    return (
+        <Fragment>
+            <ErrorModal show={!!error} error={error} onClear={clearError} />
+            <Modal
+                show={props.show && !error}
+                onClose={props.onClick}
+                onSubmit={onSubmitHandler}
+                headerText={isLoading ? 'Loading...' : !currentCard ? 'Create Card' : '{Card Name}'}
+                className={classes.Modal}
+                style={{ backgroundColor: themeContext.color }}
+                formStyles={{ height: '78%' }}
+                contentCls={classes.Content}
+                footerCls={classes.Footer}
+                footerNodes={
+                    isLoading ? null : (
+                        <Fragment>
+                            <Button type="submit">{!currentCard ? 'CREATE' : 'UPDATE'}</Button>
+                            <Button onClick={props.onClick} type="button" inverse>
+                                CLOSE
+                            </Button>
+                            <Button onClick={onDeleteHandler} type="button" inverse>
+                                DELETE
+                            </Button>
+                        </Fragment>
+                    )
+                }
+            >
+                <Card className={classes.Card} style={{ backgroundColor: themeContext.color }}>
+                    {isLoading && <Spinner asOverlay style={{ backgroundColor: themeContext.color }} />}
+                    {!isLoading && <p>content...</p>}
+                    {/*description*/}
+                    {/*checklists...*/}
+                </Card>
+            </Modal>
+        </Fragment>
+    );
+};
 
 export default CardModal;
--- src/cards/components/Cards.tsx
+++ src/cards/components/Cards.tsx
@@ -1,7 +1,52 @@
-// TODO
+import { Fragment, useState } from 'react';
 
+import CardModal from './CardModal';
+import CardItem from './CardItem';
+import Button from '../../common/components/Interactable/Button';
+import { BaseProps, CardResponse, Functional } from '../../common/util';
+import classes from './Cards.module.css';
 
-const Cards = () => <div>CARD</div>
+interface CardsProps extends BaseProps {
+    cards: CardResponse<string[]>[];
+    boardColor: string;
+}
 
+const Cards: Functional<CardsProps> = (props) => {
+    const [isViewingCardId, setIsViewingCardId] = useState<string | null>(null);
+    const [isCreatingCard, setIsCreatingCard] = useState<boolean>(false);
+
+    const onCreateCardAccept = (e: any) => {
+        setIsCreatingCard(true);
+        setIsViewingCardId(null);
+    };
+
+    const onViewCardAccept = (id: string) => {
+        setIsViewingCardId(id);
+        setIsCreatingCard(false);
+    };
+
+    const onViewCardDeny = (e: any) => {
+        setIsCreatingCard(false);
+        setIsViewingCardId(null);
+    };
+
+    return (
+        <Fragment>
+            <CardModal cardId={isViewingCardId} show={!!isViewingCardId || isCreatingCard} onClick={onViewCardDeny} />
+            <div className={classes.Container}>
+                <div className={classes.Cards}>
+                    {props.cards.map((card) => (
+                        <div key={card._id} onClick={onViewCardAccept.bind(null, card._id)}>
+                            <CardItem />
+                        </div>
+                    ))}
+                </div>
+                <Button inverse onClick={onCreateCardAccept}>
+                    ADD CARD
+                </Button>
+            </div>
+        </Fragment>
+    );
+};
 
 export default Cards;
