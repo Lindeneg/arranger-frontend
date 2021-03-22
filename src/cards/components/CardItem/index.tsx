236f8645ad621c534b9349a12240801dcc6e9d27
Christian <christianlindeneg@gmail.com>
2021-03-22 18:48:08 +0100

    start on card items

--- src/cards/components/CardItem/index.tsx
+++ src/cards/components/CardItem/index.tsx
@@ -1,6 +1,16 @@
-// TODO
+import { BaseProps, CardResponse, Clickable, Functional } from '../../../common/util';
+import classes from './CardItem.module.css';
 
-const CardItem = () => <div></div>
+interface CardItemProps extends BaseProps, Clickable, CardResponse<string[]> {}
 
+const CardItem: Functional<CardItemProps> = (props) => {
+    return (
+        <div onClick={props.onClick} className={classes.Item}>
+            {props.name}
+            <br />
+            {props.checklists.length} checklists
+        </div>
+    );
+};
 
 export default CardItem;
--- src/cards/components/Checklists/index.tsx
+++ src/cards/components/Checklists/index.tsx
@@ -1,3 +1,15 @@
-const Checklists = () => <div></div>;
+import { BaseProps, ChecklistResponse, Functional } from '../../../common/util';
+
+interface ChecklistsProps extends BaseProps {
+    checklists: ChecklistResponse[];
+}
+
+const Checklists: Functional<ChecklistsProps> = (props) => (
+    <div>
+        {props.checklists.map((e) => (
+            <p>CheckList</p>
+        ))}
+    </div>
+);
 
 export default Checklists;
