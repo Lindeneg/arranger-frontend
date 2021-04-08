27c89221a952184e9da68792d810e86a54377efc
Christian <christianlindeneg@gmail.com>
2021-04-08 22:00:08 +0200

    add bootstrap css

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import store from './store';
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <React.StrictMode>
