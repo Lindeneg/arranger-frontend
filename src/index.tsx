4769fc4544b91682a241cd282ff797c39189a8ac
Christian <christianlindeneg@gmail.com>
2021-04-09 07:05:45 +0200

    bootstrap with csstransitionsgroup is not happy with strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -9,13 +8,11 @@ import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
