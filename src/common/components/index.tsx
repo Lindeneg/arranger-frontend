e6c9711e63b04f19217926e01bdafe1c8922ac3d
Christian <christianlindeneg@gmail.com>
2021-04-09 10:23:45 +0200

    export common components

--- src/common/components/index.tsx
+++ src/common/components/index.tsx
@@ -1,1 +1,3 @@
-export {}
+export { default as Spinner } from './Spinner/Spinner';
+export { default as ErrorModal } from './ErrorModal/ErrorModal';
+export { default as Navigation } from './Navigation/Navigation';
